760404558b9021c012428b477a1fa0a30a8aaff9
MariannaBazzani <merionbaz@gmail.com>
2024-01-23 16:43:44 +0100

    Aggiungere i file di progetto.

---
+++ Es per casa/Radio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_per_casa
+{
+    internal class Radio
+    {
+        private float volume;
+        public float Volume 
+        { 
+            get { return volume; } 
+            set { volume = value; } 
+        }
+
+        public void AumentaVolume()
+        {
+            volume *= 1.1f;
+        }
+
+        public void DiminuisciVolume()
+        {
+            volume *= 0.9f;
+        }
+    }
+}
